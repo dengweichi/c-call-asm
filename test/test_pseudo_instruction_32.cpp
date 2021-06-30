594e0aa8cab4e27c9e5bd67bf775d2ec4b8ca160
dengweichi@cmcm.com <dengweichi@cmcm.com>
2021-06-30 19:29:11 +0800

    增加伪指令的测试用例

--- test/test_pseudo_instruction_32.cpp
+++ test/test_pseudo_instruction_32.cpp
@@ -11,4 +11,28 @@ TEST(test_extern_32, test_pseudo_instruction_32) {
     testExternFun();
     EXPECT_EQ(externVar, 1);
 }
+
+extern "C" {
+    extern int testGlobalVar;
+    extern int testGlobalFun();
+}
+
+TEST(test_global_32, test_pseudo_instruction_32) {
+    EXPECT_EQ(testGlobalVar, 1);
+    EXPECT_EQ(testGlobalFun(), 1);
+}
+
+extern "C" {
+    extern int testEquFun();
+}
+TEST(test_equ_32, test_pseudo_instruction_32) {
+    EXPECT_EQ(testEquFun(), 6);
+}
+
+extern "C" {
+    extern int testTimesFun();
+}
+TEST(test_times_32,test_pseudo_instruction_32){
+    EXPECT_EQ(testTimesFun(), 10);
+}
 #endif
--- test/test_pseudo_instruction_32.s
+++ test/test_pseudo_instruction_32.s
@@ -8,7 +8,43 @@ section .data
 section .text
 global testExternFun
 testExternFun:
+    mov dword [externVar],1
+    ret
+section .text
+
+
+section .data
+global testGlobalVar
+testGlobalVar dd 1
+section .data
+
+section .text
+global testGlobalFun
+testGlobalFun:
     mov eax,1
-    mov [externVar],eax
+    ret
+section .text
+
+section .data
+equArray db '123456'
+equArrayLength EQU $-equArray
+section .data
+
+section .text
+global testEquFun
+testEquFun:
+    mov eax, equArrayLength
+    ret
+section .text
+
+section .data
+timesArray times 10 db '1'
+timesArrayLength EQU $-timesArray
+section .data
+
+section .text
+global testTimesFun
+testTimesFun:
+    mov eax, timesArrayLength
     ret
 section .text
